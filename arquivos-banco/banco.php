8a628d313e01d2d52a616566c1f53726760afd6d
bruno <braphaelperez98@gmail.com>
2024-01-12 17:22:18 -0300

    fixed

--- arquivos-banco/banco.php
+++ arquivos-banco/banco.php
@@ -1,41 +1,42 @@
 <?php
 
-//funções: =>
-
-
-
-// => array das contas correntes:
 $contasCorrentes = [
-    '123.456.789.10' => [
-        'titular' => 'Bruno Perez',
-        'saldo' => 1000,
+    '123.456.789-10' => [
+        'titular' => 'Maria',
+        'saldo' => 10000
     ],
-    '023.320.450-79' => [
-        'titular' => 'Lucas Maciel',
-        'saldo' => 300,
+    '123.456.689-11' => [
+        'titular' => 'Alberto',
+        'saldo' => 300
     ],
-    '055.212.432-21' => [
-        'titular' => 'Pedro da sailva',
-        'saldo' => 1000,
-    ],
-    '012.321.534-44' => [
-        'titular' => 'Claudia dos santos',
-        'saldo' => 2000,
+    '123.256.789-12' => [
+        'titular' => 'Vinicius',
+        'saldo' => 100
     ]
-
 ];
 
-//para cada conta corrente como cpf (key) armazenar como conta.
+
+
+$contasCorrentes['123.456.789-10'] = sacar(
+    $contasCorrentes['123.456.789-10'],
+    500
+);
+
+$contasCorrentes['123.456.689-11'] = sacar(
+    $contasCorrentes['123.456.689-11'],
+    200
+);
+
+$contasCorrentes['123.256.789-12'] = depositar(
+    $contasCorrentes['123.256.789-12'],
+    900
+);
+
 foreach ($contasCorrentes as $cpf => $conta) {
     exibeMensagem(
-        "$cpf {$conta['titular']} {$conta['saldo']}"
-        //para exibit mensagens de array associativo deve-se utilizar desta maneira;
-    );
+    "$cpf $conta[titular] {$conta[saldo]");
 }
 
 
-$contasCorrentes['023.320.450-79'] = sacar($contasCorrentes['023.320.450-79'], 100);
 
-$contasCorrentes['123.456.789.10'] = sacar($contasCorrentes['123.456.789.10'], 100);
 
-$contasCorrentes['055.212.432-21'] = depositar($contasCorrentes['055.212.432-21'], 200);
